53899097783812a5456bddb4f17d93b21c1a7f06
Robi <robi.maliqi@hotmail.co.uk>
2022-07-08 14:08:57 +0100

    Test-driving the NotesView class; Challenge; Test-drive a new class NotesView, similar to the View class seen in the previous example.

---
+++ notesView.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ const fs = require('fs');
+ const NotesModel = require('./notesModel');
+ const NotesView = require('./notesView');
+ 
+ describe('Notes view', () => {
+   it('displays 2 notes', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+ 
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    model.addNote('A first note');
+    model.addNote('Another one');
+
+    view.displayNotes();
+ 
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+   });
+});
+
+module.exports = NotesView;
